52869b032ba25ef8fc707cb515459349696e9904
Franklin Nwankwo <franklin.nwankwo@gmail.com>
2012-03-16 00:20:31 +0000

    Quadroid main file

---
+++ QuadroidApp/src/quadroidapp/QuadroidApp.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quadroidapp;
+
+import javax.sound.midi.SysexMessage;
+
+/**
+ *
+ * @author oniox
+ */
+public class QuadroidApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("Testing");
+    }
+}
